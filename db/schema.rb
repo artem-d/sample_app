bb80a24537b7811f052065eb126e737796ee7130
Artem Derevianko <artem.d.6@gmail.com>
2013-06-16 13:43:53 +0300

    Exercises chapter 7 done

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,6 @@ ActiveRecord::Schema.define(version: 20130604194717) do
     t.string   "password_digest"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
