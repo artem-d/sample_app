467cfc913a673e81f021e41d6032198bbbd5783d
Artem Derevianko <artem.d.6@gmail.com>
2013-06-21 13:44:06 +0300

    Chapter 8 exercises done

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -47,24 +47,29 @@ describe "User pages" do
     end
 
     describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
+      # before do
+      #   fill_in "Name",         with: "Example User"
+      #   fill_in "Email",        with: "user@example.com"
+      #   fill_in "Password",     with: "foobar"
+      #   fill_in "Confirmation", with: "foobar"
+      # end
+
+      # it "should create a user" do
+      #   expect { click_button submit }.to change(User, :count).by(1)
+      # end
 
       it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
+        expect { valid_signup }.to change(User, :count).by(1)
       end
       
       describe "after saving the user" do
-        before { click_button submit }
+        before { valid_signup }
         let(:user) { User.find_by_email('user@example.com') }
 
         it { should have_link('Sign out', href: signout_path) }
         it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        # it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_success_message('Welcome') }
       end
     end
   end
